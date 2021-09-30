242f02d1c29cb3956677ed03f34ad2d95391625c
redcross16 <belkill@mail.ru>
2021-09-30 23:25:52 +0300

    OOP JS lesson 3

--- Lesson_4/Test.js
+++ Lesson_4/Test.js
@@ -1,6 +1,18 @@
 class Test
 {
-    set testProp(){
-        this.testProp() =
+    constructor() {
+
+    }
+    // setTestProp(name){
+    //     this._name = name.trim().toLowerCase();
+    // }
+    getName() {
+        return this._name;
+    }
+    set testProp(testProp) {
+        this._testProp = testProp;
+    }
+    set name(name) {
+        this._name = name.trim().toLowerCase();
     }
 }
---
+++ Lesson_4/app.js
@@ -0,0 +1,8 @@
+const test = new Test();
+
+
+
+// test.setTestProp('Alex')
+test.name = ' 123';
+test.testProp = 'background-color: red';
+console.log(test.getName())
